9e36ecba1e3cc2540aab1d67a504c3cb8d39f87f
Edwin Henneken <ehenneken@cfa.harvard.edu>
2013-05-29 10:15:09 -0400

    __init__: normalized keywords get loaded in local_config

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -5,9 +5,3 @@ from recommender_utils import get_suggestions
 
 _basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-IDENTIFIER_FILE = _basedir + '/data/ASTkeywords.set'
-try:
-    IDENTIFIERS = open(IDENTIFIER_FILE).read().strip().split('\n')
-except:
-    IDENTIFIERS = []
-
